a98060f4d95e9eedfcbc08ff6a52c82abaffeff4
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-02-12 10:58:53 -0500

    Feat: added routes to app

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 
 // DB Routes
+const productRoutes = require('./routes/products.js');
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +11,8 @@ const app = express();
 // Parse incoming JSON & form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/products', productRoutes);
 
 //Server
 app.listen(port, () => {
