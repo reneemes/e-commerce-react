0351c963ebae72aaf81d21f4f3f8f004ed0fb93e
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-02-14 16:47:14 -0500

    Feat: added slider, fixed mobile nav overlay with slider, clicking outside the nav bar and navigating will close the nav bar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,19 +2,31 @@ import './Header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  // const navRef = useRef(null);
+  const closeNav = () => setIsOpen(false);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (navRef.current && !navRef.current.contains(e.target)) {
+        closeNav();
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
     <header className='header'>
-      <Link className='header__title' to={'/'}>Bagel Bites</Link>
-      <nav className='nav nav--desktop' id='desktop-nav'>
-        <Link className='header__product-btn' to={'/shop'}>Shop Bagels</Link>
-        <Link className='header__product-btn' to={'/contact'}>Contact Us</Link>
-      </nav>
-
       <FontAwesomeIcon 
         icon={faBars} 
         size='2xl' 
@@ -22,6 +34,11 @@ function Header() {
         onClick={() => setIsOpen(true)}
         id='hamburger-icon'
       />
+      {/* Overlay */}
+      {isOpen && (
+        <div className="overlay" onClick={closeNav} />
+      )}
+      {/* Mobile */}
       <nav className={`header-nav nav nav--mobile ${isOpen ? 'nav-open' : ''}`} id='mobile-nav'>
         <FontAwesomeIcon 
           icon={faX} 
@@ -30,8 +47,21 @@ function Header() {
           onClick={() => setIsOpen(false)}
           id='close-btn'
         />
-        <Link className='header-nav__product-btn' to={'/shop'}>Shop Bagels</Link>
-        <Link className='header-nav__contact-btn' to={'/contact'}>Contact Us</Link>
+        <Link className='header-nav__product-btn' to={'/shop'} onClick={() => setIsOpen(false)}>
+          Shop Bagels
+        </Link>
+        <Link className='header-nav__contact-btn' to={'/contact'} onClick={() => setIsOpen(false)}>
+          Contact Us
+        </Link>
+      </nav>
+
+      {/* Title */}
+      <Link className='header__title' to={'/'}>Bagel Bites</Link>
+
+      {/* Desktop */}
+      <nav className='nav nav--desktop' id='desktop-nav'>
+        <Link className='header__product-btn' to={'/shop'}>Shop Bagels</Link>
+        <Link className='header__product-btn' to={'/contact'}>Contact Us</Link>
       </nav>
     </header>
   )
--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,19 +1,41 @@
 import './HomePage.scss';
+import '@splidejs/splide/dist/css/splide.min.css';
+// import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 import { Link } from 'react-router-dom';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 
 function HomePage() {
 
   return (
-    <div className="main-body">
-      <section className="main-body__hero">
-        <img className="main-body__hero--img" src="/assets/bagel-hero.jpg" alt="Bagels"/>
-        <div className="main-body__hero--txt">
-          <p>Bagel Bites</p>
+    <div className='main-body'>
+
+
+      <div className='main-body__hero'>
+
+        <Splide
+          options={ {
+            rewind: true,
+            height: 400,
+          } }  
+        >
+          <SplideSlide>
+            <img className='main-body__hero--img' src='/assets/bagel-hero.jpg' alt='Bagels photo'/>
+          </SplideSlide>
+          <SplideSlide>
+            <img className='main-body__hero--img' src='/assets/classic-spread.jpg' alt='Bagel spread photo'/>
+          </SplideSlide>
+          <SplideSlide>
+            <img className='main-body__hero--img' src='/assets/bagel-hero2.avif' alt='Bagels and jam photo'/>
+          </SplideSlide>
+        </Splide>
+
+        <div className='main-body__hero--txt'>
+          <h1>Bagel Bites</h1>
         </div>
-      </section>
-      <section className="main-description">
-        <p className="main-description__greeting">Welcome to Bagel Bites, where every bagel is freshly baked and bursting with flavor!</p>
-        <p className="main-description__about">
+      </div>
+      <section className='main-description'>
+        <p className='main-description__greeting'>Welcome to Bagel Bites, where every bagel is freshly baked and bursting with flavor!</p>
+        <p className='main-description__about'>
           From classic favorites like Everything and Blueberry to bold creations like
           Jalapeño Cheddar and Pizza Bagels, we’ve got something to satisfy every craving.
           Pair them with our creamy spreads or fresh toppings for the perfect 
@@ -22,9 +44,9 @@ function HomePage() {
           and let your taste buds celebrate!
         </p>
       </section>
-      <section className="main-links">
-        <Link className="main-links__btn" to={'/shop'}>Shop Bagels</Link>
-        <Link className="main-links__btn" to={'/contact'}>Contact Us</Link>
+      <section className='main-links'>
+        <Link className='main-links__btn' to={'/shop'}>Shop Bagels</Link>
+        <Link className='main-links__btn' to={'/contact'}>Contact Us</Link>
       </section>
     </div>
   )
