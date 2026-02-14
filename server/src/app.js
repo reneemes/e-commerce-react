d5af8434ea97d3e1224112083fba1ea1377366f5
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-02-14 17:34:34 -0500

    Fix: frontend link

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 
